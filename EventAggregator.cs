b9ac4e23f4b89f9011d53dca1019787608875895
Marek Kacprzyk <kacprzyk.marek@Gmail.com>
2020-03-16 20:12:14 +0100

    Add Subscriber in event aggregator

--- EventAggregator.cs
+++ EventAggregator.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventAggregator
 {
-    class EventAggregator
+    class EventAggregator : IEventAggregator
     {
+
+        public EventAggregator()
+        {
+
+        }
+
+        public void Publish<TEventType>(TEventType eventType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable Subscribe(object subscriber)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private class Subscriber : IDisposable
+        {
+            private readonly object _item;
+            private readonly Action<object> _unsubscribe;
+
+            public Subscriber(object item, Action<object> unsubscribe)
+            {
+                _item = item;
+                _unsubscribe = unsubscribe;
+            }
+
+            public void Dispose()
+            {
+                _unsubscribe.Invoke(_item);
+            }
+        }
     }
 }
