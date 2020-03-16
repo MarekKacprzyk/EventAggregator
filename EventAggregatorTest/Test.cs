5ed097b319a0ee89cb519549d410dfd90d561d76
Marek Kacprzyk <kacprzyk.marek@Gmail.com>
2020-03-16 20:04:13 +0100

    Init tests

---
+++ EventAggregatorTest/Test.cs
@@ -0,0 +1,38 @@
+﻿namespace EventAggregatorTest
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Test
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void SubscribeTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PublishTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void UnsubscribeTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void PublishManyCallbackTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
