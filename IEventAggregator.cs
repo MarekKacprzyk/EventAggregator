c184cb87a5a237391c0e79dcd79b249f089ea3e0
Marek Kacprzyk <kacprzyk.marek@Gmail.com>
2020-03-16 19:58:27 +0100

    Init project

---
+++ IEventAggregator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EventAggregator
+{
+    public interface IEventAggregator
+    {
+        IDisposable Subscribe(object subscriber);
+        void Publish<TEventType>(TEventType eventType);
+    }
+}
