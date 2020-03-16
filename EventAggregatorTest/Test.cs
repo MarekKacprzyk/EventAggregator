5284a637f48fb619ab12fd14436fe127d1e60e02
Marek Kacprzyk <kacprzyk.marek@Gmail.com>
2020-03-16 20:40:19 +0100

    Add subscribe test

--- EventAggregatorTest/Test.cs
+++ EventAggregatorTest/Test.cs
@@ -1,20 +1,36 @@
 ﻿namespace EventAggregatorTest
 {
     using NUnit.Framework;
+    using EventAggregator;
+    using NSubstitute;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System;
+    using System.Linq;
 
     [TestFixture]
     public class Test
     {
+        EventAggregator _sut;
+        ISubscriber<int> _client;
+
         [SetUp]
         public void SetUp()
         {
-
+            _sut = new EventAggregator();
+            _client = Substitute.For<ISubscriber<int>>();
         }
 
         [Test]
         public void SubscribeTest()
         {
-            Assert.Fail();
+            _sut.Subscribe(_client);
+            var dictionary = GetEventAggregatorDictionary();
+            var subscribers = dictionary
+                .SelectMany(d => d.Value)
+                .Select(c => c.Target).ToArray();
+
+            Assert.That(subscribers, Has.Member(_client));
         }
 
         [Test]
@@ -33,6 +49,12 @@
         public void PublishManyCallbackTest()
         {
             Assert.Fail();
+        }
+
+        private Dictionary<Type, List<WeakReference>> GetEventAggregatorDictionary()
+        {
+            var dictionary = _sut.GetType().GetField("_eventDictionary", BindingFlags.NonPublic | BindingFlags.Instance);
+            return (Dictionary<Type, List<WeakReference>>)dictionary.GetValue(_sut);
         }
     }
 }
